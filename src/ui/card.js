22d28f79a38764dcc5cd3620fe5ed5b1f79a1bd3
EvgeniaVishnevskaya <jeinifer.vishnevskaya@gmail.com>
2024-04-12 17:08:55 +0300

    fix: card border radius

--- src/ui/card.js
+++ src/ui/card.js
@@ -3,7 +3,7 @@ import GetImage from './cardItems'
 export default function Card ({header, description, color, hovercolor, type, image}) {
     return <div className={`${color} hover:${hovercolor} flex flex-col items-center rounded-md card cursor-pointer w-64 h-full transition-all duration-300`}>
       <div className='card-top-cover relative'>
-        <div className={`${hovercolor} image-cover-${type} card-top flex items-center justify-center transition-all duration-300 ease-linear h-full rounded-md`}>
+        <div className={`${hovercolor} image-cover-${type} card-top flex items-center justify-center transition-all duration-300 ease-linear h-full rounded-t-md`}>
             <div className='z-10'>
               <GetImage name={image} />
             </div>
